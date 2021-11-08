e0804334fd1523da2692181d28253c62a0f8cffc
JoaoVitorA <joaoarau2004@gmail.com>
2021-11-07 22:31:33 -0300

    Last adjustiment

--- index.php
+++ index.php
@@ -158,7 +158,6 @@ input[type=date]:focus::-webkit-datetime-edit {
                     <button type="submit" class="btn btn-dark" value="Salvar" id="btn1"> Enviar </button>
                     <button type="reset" class="btn btn-dark" value="Limpar" id="btn2"> Limpar </button>
                 </div>
-                sim as vez
             </div>
         </div>
     </form>
